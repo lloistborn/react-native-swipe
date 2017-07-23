8b476cef444bff2dbc2aefee74bd8f3849ec3c79
Faisal Morensya <lloistborn@live.com>
2017-07-23 21:33:32 +0700

    init & card render without scroll controll

---
+++ index.android.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
+} from 'react-native';
+import { Card, Button } from "react-native-elements";
+import Deck from "./src/component/Deck";
+
+const DATA = [
+	{ id: 1, text: 'Card #0', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+	{ id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+	{ id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+	{ id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+	{ id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+	{ id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+	{ id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+	{ id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
+
+export default class swipe extends Component {
+	renderCard(item) {
+		return(
+			<Card
+				key = { item.id }
+				title = { item.text }
+				image = {{ uri: item.uri }}
+			>
+				<Text style = {{ marginBottom: 10 }}>
+					Custom this text later.
+				</Text>
+
+				<Button
+					icon = {{ name: "code" }}
+					backgroundColor = "#03A9F4"
+					title = "View Now!"
+				/>
+			</Card>
+		);
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>			
+				<Deck 
+					data = { DATA }
+					renderCard = { this.renderCard }
+				/>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#F5FCFF',
+	},
+});
+
+AppRegistry.registerComponent('swipe', () => swipe);
