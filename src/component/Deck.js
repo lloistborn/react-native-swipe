dd2ab216511e9a22bb2fd932d11cd00c5c9fb883
lloisborn <faisalmorensya@gmail.com>
2017-08-05 23:29:38 +0700

    - get screen width - reset position using animated.spring

--- src/component/Deck.js
+++ src/component/Deck.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { View, Animated, PanResponder } from "react-native";
+import { View, Animated, PanResponder, Dimensions } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends Component {
 	constructor(props) {
@@ -15,16 +17,24 @@ class Deck extends Component {
 					y: gesture.dy
 				})
 			},
-			onPanResponderRelease: () => {}
+			onPanResponderRelease: () => {
+				this.resetPosition();
+			}
 		});
 
 		this.state = {panResponder, position};
 	}
 
+	resetPosition() {
+		Animated.spring(this.state.position, {
+			toValue: {x: 0, y: 0}
+		}).start();
+	}
+
 	getCardStyle() {
 		const {position} = this.state;
 		const rotate = position.x.interpolate({
-			inputRange: [-500, 0, 500],
+			inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
 			outputRange: ['-120deg', '0deg', '120deg']
 		});
 
