a58120c1b954addac7df52b85b28886afbf5d22d
lloisborn <faisalmorensya@gmail.com>
2017-08-05 22:35:25 +0700

    attach Animated.View to every card to make it move through your finger

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
+import { Card, Button } from "react-native-elements";
+import Deck from "./src/component/Deck";
+
+const DATA = [
+	{ id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+	{ id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+	{ id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+	{ id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+	{ id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+	{ id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+	{ id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+	{ id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
 
 export default class swipe extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+	renderCard(item) {
+		return(
+			<Card
+				key = {item.id}
+				title = {item.text}
+				image = {{uri: item.uri}}
+			>
+				<Text style = {{marginBottom: 10}}>
+					{item.text}
+				</Text>
+
+				<Button
+					icon = {{ name: "code" }}
+					backgroundColor = "#03A9F4"
+					title = "View"
+				/>
+			</Card>
+		);
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>			
+				<Deck 
+					data = { DATA }
+					renderCard = { this.renderCard }
+				/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#F5FCFF',
+	},
 });
 
 AppRegistry.registerComponent('swipe', () => swipe);
--- src/component/Deck.js
+++ src/component/Deck.js
@@ -5,26 +5,39 @@ class Deck extends Component {
 	constructor(props) {
 		super(props);
 		
+		const position = new Animated.ValueXY();
+
 		const panResponder = PanResponder.create({
 			onStartShouldSetPanResponder: () => true,
 			onPanResponderMove: (event, gesture) => {
-				console.log(gesture);
+				position.setValue({
+					x: gesture.dx, 
+					y: gesture.dy
+				})
 			},
 			onPanResponderRelease: () => {}
 		});
 
-		this.state = { panResponder };
+		this.state = {panResponder, position};
 	}
 
 	renderCards() {
-		return this.props.data.map(item => {
-			return this.props.renderCard(item);
+		return this.props.data.map((item, index) => {
+			return (
+				<Animated.View
+					key = {item.id}
+					{...this.state.panResponder.panHandlers}
+					style = {this.state.position.getLayout()}
+				>
+					{this.props.renderCard(item)}
+				</Animated.View>
+			);
 		});
 	}
 
 	render() {
 		return(
-			<View { ...this.state.panResponder.panHandlers }>
+			<View>
 				{ this.renderCards() }
 			</View>
 		);
